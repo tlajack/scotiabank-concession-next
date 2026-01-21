6940923063ff527846ddce858cfd93f5c151aa04
tlaJack <jack@tlagraphics.com>
2026-01-21 14:46:09 -0500

    add cocktail page to the list of pages

--- app/page.tsx
+++ app/page.tsx
@@ -26,6 +26,9 @@ export default function Home() {
         <li>
           <Link href="/specialty-food">Specialty Offerings</Link>
         </li>
+        <li>
+          <Link href="/cocktail">Cocktail</Link>
+        </li>
       </ul>
     </main>
   );
